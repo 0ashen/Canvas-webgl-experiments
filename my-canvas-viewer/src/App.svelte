91f8469981e2c1c0710bf726a45fd202e5eb68c0
0ashen <iusfilipp@gmail.com>
2020-12-29 23:57:03 +0300

    :: svelte template viewer :: initial svelte template

---
+++ my-canvas-viewer/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let name;
+    export const pages = [
+        {path: '/dots', label: '/dots',},
+        {path: '/mario', label: '/mario',}
+    ];
+</script>
+
+<main>
+    <sidebar>
+        <nav>
+            <ul>
+                {#each pages as page}
+                    <li>
+                        <a href={page.path}>
+                            {page.label}
+                        </a>
+                    </li>
+                {/each}
+            </ul>
+        </nav>
+    </sidebar>
+</main>
+
+<style lang="scss">
+  main {
+    display: flex;
+
+    sidebar {
+      width: 200px;
+      background-color: #e2e2e2;
+      nav {
+        ul {
+          display: block;
+
+          li {
+            list-style: none;
+            display: block;
+
+            a {
+              font-size: 25px;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
